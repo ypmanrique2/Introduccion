0b3c6c2038b43cf7e3d989e269e1cbe2de277bf4
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-05-20 20:35:43 -0500

    Consolide

--- js/calcular-imc.js
+++ js/calcular-imc.js
@@ -2,7 +2,7 @@ var pacientes = document.querySelectorAll(".paciente");
 
 
 
-for(var i = 0;i< pacientes.length;i++){
+for(var i = 0;i< pacientes.length;i++) {
     var paciente = pacientes[i];
     var tdPeso = paciente.querySelector(".info-peso");
     var peso = tdPeso.textContent;
@@ -13,30 +13,46 @@ for(var i = 0;i< pacientes.length;i++){
     var tdIMC = paciente.querySelector(".info-imc");
 
 
-    pesoEsValido = true;
-    alturaEsValida = true;
+    pesoEsValido = validarPeso(peso);
+    alturaEsValida = validarAltura(altura);
 
-    if ((peso < 0) || (peso > 1000)){
+    if (!pesoEsValido) {
         console.log("Peso incorrecto");
         tdIMC.textContent = "Peso incorrecto";
         pesoEsValido = false;
         paciente.classList.add("paciente-incorrecto");
     }
 
-    if ((altura < 0) || (altura > 3.00)){
+    if (!alturaEsValida) {
         console.log("Altura incorrecta");
         tdIMC.textContent = "Altura incorrecta";
         alturaEsValida = false;
         paciente.classList.add("paciente-incorrecto");
     }
 
-    if(pesoEsValido && alturaEsValida){
+    if(pesoEsValido && alturaEsValida) {
         tdIMC.textContent = calcularIMC(peso,altura);
     }
 
 }
 
-function calcularIMC(peso,altura){
+function calcularIMC(peso,altura) {
     var imc = peso / (altura * altura);
     return imc.toFixed(2);
+}
+
+function validarPeso(peso) {
+    if (peso >= 0 && peso < 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validarAltura(altura) {
+    if (altura >= 0 && altura < 3.00) {
+        return true;
+    } else {
+        return false;
+    }
 }
